e69bb030a197cb8c4553cb2492c03dd190bfd78f
z0xyz <solicitmind@gmail.com>
2022-08-16 06:21:42 +0200

    Finally able to execute code within the text area

--- README.md
+++ README.md
@@ -3,15 +3,16 @@
 * Scratchpad to muck around with Javascript while venturing within its realms of quirkiness and deliberate or involuntary inconsistencies.
 
 # Features
-
+- [ ] Refining the Text Area
+	- [ ] The ability to Execute code existent within it.
+		- [ ] Catch possible erros of invalid code
+	- [ ] Styling it with CSS.
+- [ ] Loading files
+	- [ ] Loading local files
+	- [ ] Loading files from remote sources
+		- [ ] Catching possible errors due to unsatisfied network promises.
 - [ ] Text area to input actual code
 - [ ] Code output pane
 - [ ] Syntax highlighting
 - [ ] Common binding compilations
-
 - [ ] Code completion
-
-
-
-
-
--- main.js
+++ main.js
@@ -1,16 +1,46 @@
-const { app, BrowserWindow } = require("electron")
+const { app, BrowserWindow, ipcRenderer, ipcMain } = require("electron")
+const path = require('path')
 
+//mainWindow.webContents.executeJavaScript(doSomething)
 function createWindow(){
 	// Create the browser window
-	const window = new BrowserWindow({
+	const mainWindow = new BrowserWindow({
 		width: 800,
 		height: 600,
 		webPreferences: {
-			nodeIntegration: true 
+			nodeIntegration: true,
+			contextIsolation: false,
+			//preload: path.join(__dirname, './main.js')
 		}
 	})
+
 	// Load the index.html file of the app
-	window.loadFile('main.html')
+	//mainWindow.webContents.on("did-finish-load", ()=> { 
+	let code = `
+	function doSomething(){
+		console.log("hello world")
+		// Code running m	<script type="text/javascript" src="code-runner.js"></script>odule
+		function runCode(codeString){
+			console.log(codeString)
+			let sourceCode = document.getElementById("sourcecode-area").textContent
+			console.log(codeString)
+		}
+		let runButton = document.getElementById("run-button")
+		let sourceCode = document.getElementById("sourcecode-area")
+		runButton.addEventListener('click', function(){runCode(sourceCode.value)})
+	}
+	doSomething()`
+	// mainWindow.webContents.executeJavaScript(code, true)
+	// })
+
+	//ipcMain.on('code:run', function(event, parameters){
+	//	console.log(parameters)
+	//})
+
+	mainWindow.webContents.openDevTools()
+	//mainWindow.loadFile('main.html')
+	mainWindow.loadURL('file://' + __dirname + '/main.html')
+
 }
 
 //app.on('ready', createWindow()) 			// These two lines do the exact same thing
